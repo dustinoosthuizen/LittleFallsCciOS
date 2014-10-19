b5e1f4fa48382fad2e24e3fc2e6e95dbb634c61d
webward <dustinosthzn@gmail.com>
2014-10-19 17:37:20 +0200

    Initial Commit

---
+++ LittleFallsCciOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LittleFallsCciOS
+//
+//  Created by Dustin Oosthuizen on 2014/10/19.
+//  Copyright (c) 2014 webward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LittleFallsCciOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LittleFallsCciOS
+//
+//  Created by Dustin Oosthuizen on 2014/10/19.
+//  Copyright (c) 2014 webward. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
